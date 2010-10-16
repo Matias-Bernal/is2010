aabca82f39072498e4873dfe3ae32e1a242cb606
marcosjaule <marcosjaule@438019a7-cb73-56e9-f660-d13415c40afd>
2010-10-16 02:13:12 +0000

    javadoc de polinomio y calculadora.. Chequearlas please

--- trunk/src/fuentes/Polinomio.java
+++ trunk/src/fuentes/Polinomio.java
@@ -3,15 +3,25 @@ package fuentes;
 import java.io.*;
 import java.util.*;
 
-
+/**
+ * @author Bernal - Odorizzi - Bressan - Jaule.
+ * Clase que representa un polinomio. 
+ */
 public class Polinomio {
 
-	private Vector<Termino> terminos;
-
+	private Vector<Termino> terminos; // usado para almacenar y manipular los terminos del polinomio
+	
+	/**
+	* Constructor de la clase Polinomio(): prepara el vector para poder empezar a usarlo en la representacion del polinomio.
+	*/
 	public Polinomio() {
 		terminos = new Vector<Termino>();
 	}
 
+	/**
+	* makePolinomioFromFile(String fileName): Setea el polinomio a partir de un polinomio que se encuentra escrito en un archivo.
+	* @param filName representa el nombre del archivo donde se encuentra el polinomio.
+	*/
 	public void makePolinomioFromFile(String fileName) {
 		try {
             FileReader archivo = new FileReader(fileName);
@@ -42,6 +52,9 @@ public class Polinomio {
         }
     }
 
+	/**
+	* verPolinomio(): Muestra por consola al polinomio.
+	*/
 	public void verPolinomio(){
 		int i = 0;
 		int valor = 0;
@@ -137,6 +150,11 @@ public class Polinomio {
 		System.out.println(res);
 	}
 
+	/**
+	* addTermPolinomio(int v, int e): Agrega un termino al polinomio.
+	* @param v representa el coeficiente del termino que se desea agregar.
+	* @param e representa el exponente del termino que se desea agregar.
+	*/
 	public void addTermPolinomio(int v, int e){
 		Termino termino = new Termino();
 		termino.setValor(v);
@@ -148,6 +166,11 @@ public class Polinomio {
 			terminos.setElementAt(termino,index); //OJO QUE CUANDO EN EL ARCHIVO SE REPITEN DOS VALORES CON
 	}											  // EL MISMO EXPONETES PISA EL QUE YA HABIA ANTES CON EL ULTIMO
 
+	/**
+	* ordenarDec(Polinomio p): Ordena los terminos de un polinomio de forma decreciente.
+	* @param p representa el polinomio a ordenar.
+	* @return p ordenado decrecientemente.
+	*/
 	public Polinomio ordenarDec(Polinomio p) {
 		Polinomio res = new Polinomio();
 		Vector<Termino> pol = new Vector<Termino>(p.gerTerminos());
@@ -168,6 +191,11 @@ public class Polinomio {
 		return res;
 	}
 
+	/**
+	* completar(Polinomio p):Completa un polinomio en el caso de que le falten terminos.
+	* @param p representa el polinomio a completar.
+	* @return p completo.
+	*/
 	public Polinomio completar(Polinomio p) {
 		Polinomio polinomio = p;
 		if (!estaOrdenado(polinomio)){
@@ -211,7 +239,12 @@ public class Polinomio {
 		polinomio.setTerminos(correc);
 		return polinomio;
 	}
-
+	
+	/**
+	* estaOrdenado(Polinomio p): Indica si un polinomio esta ordenado.
+	* @param p representa el polinomio a analizar.
+	* @return True si p esta ordenado o False caso contrario.
+	*/
 	public boolean estaOrdenado(Polinomio p) {
 		boolean ordenado = true;
 		int i = 0;
@@ -227,6 +260,11 @@ public class Polinomio {
 		return ordenado;
 	}
 
+	/**
+	* grado(Polinomio p): Indica el grado de un polinomio.
+	* @param p representa el polinomio a analizar.
+	* @return int representando el grado de p.
+	*/
 	public int grado(Polinomio p) {
 		Polinomio polinomio = p;
 		if (!estaOrdenado(polinomio)){
@@ -235,6 +273,11 @@ public class Polinomio {
 		return polinomio.gerTerminos().firstElement().getExponente();
 	}
 
+	/**
+	* indexOf(int exponente): Indica la posicion de un exponente dentro de la estructura que representa al polinomio.
+	* @param exponente que se desea buscar.
+	* @return int representando la posicion del exponente dentro del la estructura que almacena los terminos.
+	*/
 	private int indexOf(int exponente) {
 		for (int i = 0; i < terminos.size(); i++) {
 			Termino termino = terminos.elementAt(i);
@@ -244,13 +287,24 @@ public class Polinomio {
 		return -1;
 	}
 
+	/**
+	* gerTerminos(): Devuelve un vector conteniendo todos los terminos del polinomio.
+	* @return Un vector con los terminos del polinomio.
+	*/
 	public Vector<Termino> gerTerminos()  {
 		return terminos;
 	}
 
+	/**
+	* setTerminos(Vector<Termino> terminos): Setea el polinomio a partir de un vector con terminos.
+	*/
 	public void setTerminos(Vector<Termino> terminos){
 		this.terminos=terminos;
 	}
+	
+	/**
+	* corregirPolinomio(): Remueve del polinomio los terminos que poseen coeficiente igual a 0.
+	*/
 	public void corregirPolinomio(){
 		int i = 0;
 		boolean fin = false;
@@ -263,6 +317,8 @@ public class Polinomio {
 			i++;
 		}
 	}
+	
+	//TODO borrar main
 	public static void main(String[] args){
 		Polinomio pol = new Polinomio();
 		Polinomio aux = new Polinomio();
